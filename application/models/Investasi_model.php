fcf4086bffaff52155e068dc8e76dfbac4ad7fe8
Ahmad Mazlan <43345584+xmazlan@users.noreply.github.com>
2021-08-13 17:20:13 +0700

    Update Investasi_model.php

--- application/models/Investasi_model.php
+++ application/models/Investasi_model.php
@@ -27,11 +27,6 @@ class Investasi_model extends CI_Model
         $this->db->select('*, tb_investasi.id as id_investasi');
         $this->db->join('tb_lokasi', 'tb_lokasi.id = tb_investasi.kode_lokasi');
         $this->db->join('tb_negara', 'tb_negara.id = tb_investasi.kode_negara');
-        // if ($jenisInvestasi == 'pmdn') {
-        //     $this->db->where('nama_negara', 'Indonesia');
-        // } elseif ($jenisInvestasi == 'pma') {
-        //     $this->db->not('nama_negara !=', 'Indonesia');
-        // }
 
         return $this->db->where('tb_investasi.id', $id)->get('tb_investasi')->row_object();
     }
